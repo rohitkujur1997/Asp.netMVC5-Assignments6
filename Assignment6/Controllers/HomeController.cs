86db5add2e225a6b17d72b8d58395124ff54f567
unknown <kujur.rohit95@gmail.com>
2022-05-03 23:09:27 +0530

    commiting assignment6

---
+++ Assignment6/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Assignment6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment6.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<User> users = new List<User>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+    }
+}
